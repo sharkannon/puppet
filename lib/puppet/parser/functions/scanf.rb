5e84f724a6555c40c6667a8bde9dd7eee9d1657b
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2015-02-10 20:55:10 +0000

    Revert "(PUP-3991) Implement scanf function for classic parser"

--- lib/puppet/parser/functions/scanf.rb
+++ lib/puppet/parser/functions/scanf.rb
@@ -1,5 +1,3 @@
-require 'scanf'
-
 Puppet::Parser::Functions::newfunction(
   :scanf,
   :type => :rvalue,
@@ -9,22 +7,17 @@ Scans a string and returns an array of one or more converted values as directed 
 See the documenation of Ruby's String::scanf method for details about the supported formats (which
 are similar but not identical to the formats used in Puppet's `sprintf` function.
 
-This function takes two mandatory arguments: the first is the String to
-convert, and the second the format String. The result of the scan is an Array,
-with each sucessfully scanned and transformed value.args The scanning stops if
-a scan is unsuccesful and the scanned result up to that point is returned. If
-there was no succesful scan at all, the result is an empty Array.
+This function takes two mandatory arguments: the first is the String to convert, and the second
+the format String. A parameterized block may optionally be given, which is called with the result
+that is produced by scanf if no block is present, the result of the block is then returned by
+the function.
 
-      scanf("42", "%i")[0] == 42
+The result of the scan is an Array, with each sucessfully scanned and transformed value.args The scanning
+stops if a scan is unsuccesful and the scanned result up to that point is returned. If there was no
+succesful scan at all, the result is an empty Array. The optional code block is typically used to
+assert that the scan was succesful, and either produce the same input, or perform unwrapping of
+the result
 
-
-When used with the future parser, an optional parameterized block may be given.  
-The block is called with the result that is produced by scanf if no block is
-present, the result of the block is then returned by the function.
-
-The optional code block is typically used to assert that the scan was
-succesful, and either produce the same input, or perform unwrapping of
-the result:
 
       "42".scanf("%i")
       "42".scanf("%i") |$x| {
@@ -34,10 +27,8 @@ the result:
         $x[0]
       }
 
-- Since 3.7.5
+- Since 4.0.0
 DOC
 ) do |args|
-  data = args[0]
-  format = args[1]
-  result = data.scanf(format)
+  function_fail(["scanf() is only available when parser/evaluator future is in effect"])
 end
